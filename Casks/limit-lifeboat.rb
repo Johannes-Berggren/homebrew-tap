604ead21e0dc298793d5789ea26f7e12a431a0a1
Johannes Berggren <johannes@berggren.co>
2026-07-15 16:43:54 +0200

    fix: bump Limit Lifeboat cask to 1.0.1

--- Casks/limit-lifeboat.rb
+++ Casks/limit-lifeboat.rb
@@ -1,6 +1,6 @@
 cask "limit-lifeboat" do
-  version "1.0.0"
-  sha256 "0691bcf240b22c8fbc77da4554eb1285ff0bf789e0d4d9ff378b5a1d7a97975c"
+  version "1.0.1"
+  sha256 "3b2ef9521b4c8d2a11a39e8e770b444a07df94f44d4d28070f6698f85fb08087"
 
   url "https://github.com/Johannes-Berggren/limit-lifeboat/releases/download/v#{version}/Limit-Lifeboat-#{version}-arm64.dmg",
       verified: "github.com/Johannes-Berggren/limit-lifeboat/"
@@ -13,6 +13,7 @@ cask "limit-lifeboat" do
     strategy :github_latest
   end
 
+  auto_updates true
   depends_on arch: :arm64
   depends_on macos: :sonoma
 
