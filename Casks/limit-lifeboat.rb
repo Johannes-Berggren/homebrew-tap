93a194664c6273de4bbc9b71a5edf63c6818ebfc
Johannes Berggren <johannes@berggren.co>
2026-07-18 14:23:38 +0200

    Update Limit Lifeboat to 1.0.4

--- Casks/limit-lifeboat.rb
+++ Casks/limit-lifeboat.rb
@@ -1,6 +1,6 @@
 cask "limit-lifeboat" do
-  version "1.0.3"
-  sha256 "b526d0a52295f5ebed62b5937c4de0dd4fb276fa65d0f16e462054f263f1043e"
+  version "1.0.4"
+  sha256 "12e06ec856a302d82641982ee4074abd01772524bb13e4416cb460cc1bfb9f23"
 
   url "https://github.com/Johannes-Berggren/limit-lifeboat/releases/download/v#{version}/Limit-Lifeboat-#{version}-arm64.dmg",
       verified: "github.com/Johannes-Berggren/limit-lifeboat/"
